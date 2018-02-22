382fa4060694eccb16dd143cc0a4f93a77370635
Francois Marelli <fran.marelli@gmail.com>
2018-02-22 11:45:11 +0100

    Added unit testing

---
+++ fprinter/tests/test_frontend.py
@@ -0,0 +1,13 @@
+import unittest
+
+import os
+import sys
+sys.path.append(os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(os.path.realpath(__file__)))))
+
+import fprinter
+
+if __name__=='__main__':
+    print('\nRunning unittest on frontend\n')
+    unittest.main()
