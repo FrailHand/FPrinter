14c50fe1613e5f49107b8b02cbe8f0385738ab6b
Francois Marelli <fran.marelli@gmail.com>
2018-03-07 14:56:07 +0100

    try to resolve import bug

--- fprinter/backend/__init__.py
+++ fprinter/backend/__init__.py
@@ -1,9 +1,8 @@
-import pyglet
-from fprinter.backend.window import Window
-
-
 def main():
     '''The main routine.'''
+
+    import pyglet
+    from fprinter.backend.window import Window
 
     print('\nLaunching the FPrinter backend\n')
 
