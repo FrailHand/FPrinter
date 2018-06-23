e470fa03bbf0fb39285478b1e44369a987c8baac
M. François <fran.marelli@gmail.com>
2018-06-23 13:18:02 +0200

    Basic logging

--- launcher.sh
+++ launcher.sh
@@ -3,6 +3,6 @@
 BIN_PATH="/fprinter/bin"
 FF_PATH="/fprinter/fprinter/frontend"
 
-${BIN_PATH}/fprinter_backend &
+${BIN_PATH}/fprinter_backend > /tmp/fprinter.log &
 sleep 3
-sudo ${BIN_PATH}/pserve ${FF_PATH}/development.ini &
+sudo ${BIN_PATH}/pserve ${FF_PATH}/development.ini > /tmp/fprinter_server.log &
