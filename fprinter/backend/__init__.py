4d602a3c41d1b37d7f394877ea8c1b1deea8f6fa
M. François <fran.marelli@gmail.com>
2018-06-23 12:02:27 +0200

    Import fix

--- fprinter/backend/__init__.py
+++ fprinter/backend/__init__.py
@@ -1,30 +1,29 @@
-import platform
-
-FULLSCREEN = True
-if platform.machine() == 'x86_64':
-    # this allows to run the code on non-raspberry machines
-    # DEVELOPMENT PURPOSE ONLY
-    import sys
-    import fake_rpi
-    from fprinter.tests import fake_rpi_serial
-    from fprinter.tests import fake_adafruit_dht
-
-    sys.modules['RPi'] = fake_rpi.RPi
-    sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO
-    sys.modules['smbus'] = fake_rpi.smbus
-    sys.modules['serial'] = fake_rpi_serial
-    sys.modules['Adafruit_DHT'] = fake_adafruit_dht
-
-    FULLSCREEN = False
-
-import signal
-import pyglet
-from fprinter.backend.manager import Manager
-from fprinter.backend.constants import Event
-
-
 def main():
     """The main routine."""
+
+    import platform
+
+    FULLSCREEN = True
+    if platform.machine() == 'x86_64':
+        # this allows to run the code on non-raspberry machines
+        # DEVELOPMENT PURPOSE ONLY
+        import sys
+        import fake_rpi
+        from fprinter.tests import fake_rpi_serial
+        from fprinter.tests import fake_adafruit_dht
+
+        sys.modules['RPi'] = fake_rpi.RPi
+        sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO
+        sys.modules['smbus'] = fake_rpi.smbus
+        sys.modules['serial'] = fake_rpi_serial
+        sys.modules['Adafruit_DHT'] = fake_adafruit_dht
+
+        FULLSCREEN = False
+
+    import signal
+    import pyglet
+    from fprinter.backend.manager import Manager
+    from fprinter.backend.constants import Event
 
     print('\nLaunching the FPrinter backend\n')
 
